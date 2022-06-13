cbe9d0c406d637efb23350afd2eb020b0f9ad218
Tzu Huan Tai <tzu.huan.tai@gmail.com>
2022-06-13 16:13:19 +0800

    fix: highcharts error and black background

--- src/app/climate/climate.component.ts
+++ src/app/climate/climate.component.ts
@@ -25,6 +25,7 @@ import { iif } from 'rxjs';
 // declare var require: any;
 // 2.
 require('highcharts/highcharts-more')(Highcharts);
+require('highcharts/themes/dark-blue')(Highcharts);
 
 @Component({
   selector: 'app-climate',
@@ -55,7 +56,8 @@ export class ClimateComponent implements OnInit, AfterContentInit {
     const optionsA: Options = {
       chart: {
         // type: 'spline'
-        zoomType: 'x'
+        zoomType: 'x',
+        backgroundColor: 'black'
       },
       title: {
         text: 'Hourly temperatures'
@@ -122,7 +124,8 @@ export class ClimateComponent implements OnInit, AfterContentInit {
     const optionsB: Options = {
       chart: {
         // type: 'spline'
-        zoomType: 'x'
+        zoomType: 'x',
+        backgroundColor: 'black'
       },
       title: {
         text: 'Hourly Humidity'
